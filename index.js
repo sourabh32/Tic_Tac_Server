89d7b926c05ef7108524832d99666d6c529133a5
sourabh@32 <sourabhsharma181003@gmail.com>
2023-12-20 12:20:26 +0530

    cors policy updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: 'true',
   })
 );
@@ -18,7 +18,7 @@ app.use(
 const io = new Server(server, {
   cors: {
     
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true
   },
